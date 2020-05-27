f5ca44de6cc17ed535ff99cc274c6e067db5d394
CDDelta <ngjunyen@gmail.com>
2020-05-27 21:44:36 +1000

    only run entrance animations when elements are... in viewport

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,28 @@
 import 'bootstrap/js/src/collapse';
+
+document.addEventListener('DOMContentLoaded', () => {
+  watchAnimationsInViewport();
+});
+
+function watchAnimationsInViewport() {
+  let observer = new IntersectionObserver(
+    (entries, _) => {
+      for (const entry of entries) {
+        if (
+          entry.isIntersecting &&
+          entry.target.classList.contains('anim-paused')
+        ) {
+          entry.target.classList.remove('anim-paused');
+          observer.unobserve(entry.target);
+        }
+      }
+    },
+    {
+      threshold: 0.3,
+    },
+  );
+
+  observer.observe(document.querySelector('.jumbotron'));
+  observer.observe(document.querySelector('#projects'));
+  observer.observe(document.querySelector('#blog'));
+}
